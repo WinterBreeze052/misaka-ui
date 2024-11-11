f8958bf38ae8ba486a227a1a441ea9bb54b22ed0
今夜月色很美 <591762041@qq.com>
2024-11-11 19:38:30 +0800

    fix(custom): lint:fix --2

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import Theme from 'vitepress/theme'
-import SSYUI from '../../../src/entry'
+import SSYUI from '../../../ui/src/entry'
 
 // 主题样式
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
